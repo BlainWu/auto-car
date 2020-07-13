4af2946b552b2091e032493574e063ba673312b3
peilin <896511142@qq.com>
2020-07-13 20:09:09 +0800

    update

---
+++ Collect_Data.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+'''=================================================
+@Project -> File   ：codes -> Collect_Data
+@IDE    ：PyCharm
+@Author ：Blain Wu
+@Date   ：2020/7/13 20:05
+@Desc   ：
+=================================================='''
+import os
+
+#os.path.split(os.path.realpath(__file__)) = ('path','Collect_Data.py')
+path = os.path.split(os.path.realpath(__file__))[0] + "/.."
